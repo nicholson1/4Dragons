2291b32869e3abe047bb2edf59fb38eb6ab99178
Neil Nicholson <neil.nicholson.ny@gmail.com>
2024-02-26 00:29:43 -0500

    idk im tired

--- Assets/RelicTester.cs
+++ Assets/RelicTester.cs
@@ -1,26 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class RelicTester : MonoBehaviour
 {
 
-    public TextMeshProUGUI InputField;
+    public Slider InputField;
+
+    private void Update()
+    {
+        this.GetComponentInChildren<TextMeshProUGUI>().text = "relic : " + (int)InputField.value;
+    }
 
     public void Click()
     {
-        if (int.TryParse(InputField.text, out int intValue))
-        {
-            Debug.Log("Integer Value: " + intValue);
-            RelicManager._instance.GetRelic(intValue);
-        }
-        else
-        {
-            Debug.LogError("Failed to parse string as integer.");
-        }
-        
+        RelicManager._instance.GetRelic((int)InputField.value);
+
+        // if (int.TryParse(InputField.text, out int intValue))
+        // {
+        //     Debug.Log("Integer Value: " + intValue);
+        //     RelicManager._instance.GetRelic(intValue);
+        // }
+        // else
+        // {
+        //     Debug.LogError("Failed to parse string as integer.");
+        // }
+
     }
 }
