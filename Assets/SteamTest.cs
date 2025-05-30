1c2219efc3de2413ed972c03d7db38280c6ea3d3
decko87 <37547029+decko87@users.noreply.github.com>
2025-05-30 16:06:44 -0400

    Fix disable steamworks scripting symbol NOTE: PLEASE ENSURE DISABLESTEAMWORKS SCRIPTING SYMBOL IS IN ANDROID PLAYER SETTINGS

--- Assets/SteamTest.cs
+++ Assets/SteamTest.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+#if !DISABLESTEAMWORKS
 using Steamworks;
+#endif
 using UnityEngine;
 
 public class SteamTest : MonoBehaviour
 {
-    
+#if !DISABLESTEAMWORKS
     void Start() {
         if(SteamManager.Initialized) {
             string name = SteamFriends.GetPersonaName();
             Debug.Log(name);
         }
     }
-    
+#endif
 }
